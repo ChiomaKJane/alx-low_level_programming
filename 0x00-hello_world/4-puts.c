02bb0bc0ad26d624435ce6337a5c70cebde375c9
chiomakjane <chiomakjane>
2022-06-30 06:29:24 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,2 @@
 #!/bin/bash
-#include <stdio.h>
-int main(void)
-{
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
-}
-
+include <stdio.h> main() { puts("Programming is like building a multilingual puzzle\n"); return (0); }
