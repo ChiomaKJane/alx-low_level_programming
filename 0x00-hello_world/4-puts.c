a380266be59c5ceb08e9fcb9c5dbda0a9ef1a089
chiomakjane <chiomakjane>
2022-06-30 07:25:10 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Description: prints "Programming is like building a multilingual puzzle"
+ *
+ *     *
+ *
+ *      * Return: Always 0 (Success)
+ *
+ *       */
+
 int main(void)
+
 {
-	puts("Programming is like building a multilingual puzzle\n");
-	Return (0);
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
 
 }
