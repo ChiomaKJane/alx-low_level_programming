9c39bb84f8ee61e7fb58eeaabb8c8b3fbe142ffe
chiomakjane <chiomakjane>
2022-07-04 11:26:50 -0700

    Task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,19 +9,20 @@
  */
 int main(void)
 {
-unsigned char a = '0';
-int i;
-for (i = 0; i < 10; i++)
+/**
+ * hexidecimal
+ * 0 1 2 3 4 5 6 7 8 9 a b c d e f
+ */
+char ch;
+int n;
+for (n = 48; n <= 57; n++)
 {
-putchar(a);
-a++;
+putchar(n);
 }
-a = '1';
-for (i = 0; i > 6; i++)
+for (ch = 'a'; ch <= 'f'; ch++)
 {
-putchar('0' + a);
-a++;
+putchar(ch);
 }
-putchar('\n');
+putchar(10);
 return (0);
 }
