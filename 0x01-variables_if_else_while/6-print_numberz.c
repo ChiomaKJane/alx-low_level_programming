844b11255fc2cbe6159f7386dd720df3509ea530
chiomakjane <chiomakjane>
2022-07-04 10:15:20 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: prints all single digit numbers of base 10 starting from 0
+ * followed by a new line
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+int a = 0;
+for (; a < 10 ; a++)
+putchar('0' + a);
+putchar('\n');
+return (0);
+}
