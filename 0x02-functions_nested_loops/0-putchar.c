a5b01fd2ec8f8a7e6b5cf1eb8c829807b9659039
chiomakjane <chiomakjane>
2022-07-05 18:30:52 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,15 +10,13 @@
  */
 int main(void)
 {
-_putchar("_");
-_putchar("p");
-_putchar("u");
-_putchar("t");
-_putchar("c");
-_putchar("h");
-_putchar("a");
-_putchar("r");
-_putchar("\n");
+char text[8] = "putchar";
+unsigned int i = 0;
+for (i = 0; i < 7; i++)
+{
+_putchar(text[i]);
+}
+_putchar('\n');
 
 return (0);
 }
